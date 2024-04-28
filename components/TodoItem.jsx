1cf09e30c712fa409f2ce97db987e33281a5fd7f
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-04-28 17:17:36 +0300

    Added delete icon the tasks

--- components/TodoItem.jsx
+++ components/TodoItem.jsx
@@ -1,19 +1,27 @@
-import {  StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
-export default function TodoItem({todo, pressHandler}) {
+export default function TodoItem({ todo, pressHandler }) {
   return (
-    <TouchableOpacity onPress={() => pressHandler(todo.key)} >
-        <Text style={styles.item}>{todo.text}</Text>
+    <TouchableOpacity onPress={() => pressHandler(todo.key)}>
+      <View style={styles.item}>
+        <MaterialIcons name="delete" size={20} color="black" />
+        <Text style={styles.itemText}>{todo.text}</Text>
+      </View>
     </TouchableOpacity>
-  )
+  );
 }
 const styles = StyleSheet.create({
-    item:{
-        padding:16,
-        marginTop:16,
-        borderColor:'#bbb',
-        borderWidth:1,
-        borderStyle:'dashed',
-        borderRadius:10
-    }
-})
+  item: {
+    flexDirection:'row',
+    padding: 16,
+    marginTop: 16,
+    borderColor: "#bbb",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 10,
+  },
+  itemText:{
+    marginLeft:10
+  }
+});
